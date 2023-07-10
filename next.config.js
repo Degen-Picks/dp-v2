6ab421835370a270d843fd210b4bf88cc6431f33
mattdegods <matthewcpfeifer@gmail.com>
2023-07-09 22:02:10 -0400

    Feat: add allowed image domains to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "i.imgur.com",
+      "pbs.twimg.com",
+      "cdn.freebiesupply.com",
+      "shdw-drive.genesysgo.net",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
