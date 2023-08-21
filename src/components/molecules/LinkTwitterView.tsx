f896eaa712e43474e4050be632b9cdcb44adb811
mattdegods <matthewcpfeifer@gmail.com>
2023-08-20 22:23:33 -0400

    Feat: add logo to connect twitter info modal

--- src/components/molecules/LinkTwitterView.tsx
+++ src/components/molecules/LinkTwitterView.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { TwitterLoginButton } from "@/components";
 import { ModalView } from "../organisms/RulesModal";
+import Image from "next/image";
 
 interface Props {
   modalView: ModalView;
@@ -13,6 +14,12 @@ const LinkTwitterView: FC<Props> = ({ modalView }) => {
       w-full pt-4 pb-8 text-center gap-5
       flex flex-col items-center justify-center`}
     >
+      <Image
+        src="/images/icons/twitter_dp.png"
+        width={95}
+        height={50}
+        alt="connect your twitter to DP"
+      />
       <p className="text-xl sm:text-2xl font-base-b text-center">
         Link Twitter / X
       </p>
