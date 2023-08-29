c0eb4e2a6bee4657abb7ec229f2ca4f844248968
Matt <gr8bitmail@gmail.com>
2023-08-28 20:49:33 -0600

    added new url paths for real

--- next.config.js
+++ next.config.js
@@ -9,6 +9,32 @@ const nextConfig = {
       "shdw-drive.genesysgo.net",
     ],
   },
+  rewrites() {
+    return {
+        beforeFiles: [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'app.degenpicks.xyz',
+                    },
+                ],
+                destination: '/classic/:path*',
+            },
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'app.degen-picks-git-development-degenpicks.vercel.app',
+                    },
+                ],
+                destination: '/classic/:path*',
+            },
+        ]
+    }
+  }
 };
 
 module.exports = nextConfig;
