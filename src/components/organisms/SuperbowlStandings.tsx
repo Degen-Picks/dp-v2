3629802543ce6c6bf409939f62ff653c5ba0fb24
mattdegods <matthewcpfeifer@gmail.com>
2024-02-07 20:37:08 -0500

    Fix: start standings page at top of screen

--- src/components/organisms/SuperbowlStandings.tsx
+++ src/components/organisms/SuperbowlStandings.tsx
@@ -40,6 +40,10 @@ const SuperbowlStandings: FC<Props> = ({
     }
   }, [currentPick]);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       {celebrateSubmit ? (
